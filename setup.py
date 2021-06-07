79cbcd6b33775b81c0642d854ad0bb4dbdc4573f
Shakshi3104 <watchosastrom3104@gmail.com>
2021-06-07 11:27:31 +0900

    Update macos version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tfgarden",
-    version="0.3.1",
+    version="0.3.2-macos",
     author="Shakshi3104",
     description="TensorFlow model Garden for HASC",
     url="https://github.com/Shakshi3104/tfgarden",
@@ -17,6 +17,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3 :: Only',
     ],
     python_requires=">=3.6, <4",
-    install_requires=["tensorflow"],
     package_dir={'': 'src'},
 )
